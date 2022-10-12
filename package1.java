f25fe527d80abf966957dc9e0e3e8eb4e8115c06
Krushnalipatil04 <99853766+Krushnalipatil04@users.noreply.github.com>
2022-10-12 09:06:37 +0530

    Add files via upload

---
+++ package1.java
@@ -0,0 +1,8 @@
+package sybca_b;
+public class package1
+{
+public void show()
+{
+System.out.println("sybca package is called");
+}
+}
---
+++ package2.java
@@ -0,0 +1,9 @@
+import sybca_b.*;
+ class package2
+{
+public static void main(String args[])
+{
+package1 d=new package1();
+d.show();
+}
+}
